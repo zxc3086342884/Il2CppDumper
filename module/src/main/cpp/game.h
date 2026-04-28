97093562c744d5188b2a299082c07cf48625b07f
zxc3086342884 <103989543+zxc3086342884@users.noreply.github.com>
2026-04-28 14:48:25 +0800

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,8 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-// 移除固定包名定义
-// 改为定义一个编译时常量控制是否过滤包名
-#define TARGET_ALL_PACKAGES true
+#define GamePackageName "com.game.packagename"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H

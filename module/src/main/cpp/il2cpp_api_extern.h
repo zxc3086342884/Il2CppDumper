fa5c9bd5290a61242f7f26e786e1fe3b998c3381
zxc3086342884 <103989543+zxc3086342884@users.noreply.github.com>
2026-05-01 21:51:38 +0800

    Fix function pointer declarations in il2cpp_api_extern.h

---
+++ module/src/main/cpp/il2cpp_api_extern.h
@@ -0,0 +1,23 @@
+#ifndef IL2CPP_API_EXTERN_H
+#定义 IL2CPP_API_EXTERN_H
+
+//其他文件可以通过外*****
+/这些函数指针在 il 2 cpp_dump. c++you you you
+
+非住院医生 int（*il 2cpp_is_vm_thread）（void*）；(*il2cpp_is_vm_thread)(void*);
+非住院医生 void**il2cpp_domain_get）（）；(*il2cpp_domain_get)();
+非住院医生 void**il 2cpp_domain_get_assemblies）（void*，size_t*）；(*il2cpp_domain_get_assemblies)(void*, size_t*);
+非住院医生无效*2cpp_assembly_get_image）（void*）；(*il2cpp_assembly_get_image)(void*);
+非住院医生 char moto**il 2 cpp_image_get_name）（void*）；(*il2cpp_image_get_name)(void*);
+非住院医生大小_t*il2cpp_image_get_class_count）（void*）；(*il2cpp_image_get_class_count)(void*);
+非住院医生无效*2cpp_image_get_class）（void*，size_t）；(*il2cpp_image_get_class)(void*，大小_t);
+非住院医生 char moto**il 2 cpp_class_get_name）（void*）；(*il2cpp_class_get_name)(void*);
+非住院医生炭素**il 2cpp_class_get_namespace）（void*）；(*il2cpp_class_get_namespace)(void*);
+非住院医生无效*2cpp_class_get_methods）（void*，void**）；(*il2cpp_class_get_methods)(无效**);
+非住院医生无效*2cpp_class_get_fields）（void*，void**）；(*il2cpp_class_get_fields)(无效**);
+非住院医生无效*2cpp_class_get_properties）（void*，void**）；(*il2cpp_class_get_properties)(无效**);
+非住院医生 char moto**il 2 cpp_method_get_name）（void*）；(*il2cpp_method_get_name)(void*);
+非住院医生 char moto**il 2 cpp_field_get_name）（void*）；(*il2cpp_field_get_name)(void*);
+非住院医生 void*(*il2cpp_field_get_type)(void*);
+
+#endif
